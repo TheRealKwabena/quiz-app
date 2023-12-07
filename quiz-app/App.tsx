6ef76a991bb3ffee94e840061eba62881f1dd9cf
Kwabena Asare <kwabenaa@net.chalmmers.se>
2023-12-07 17:51:35 +0100

    feat: added available exams box to home screen

--- quiz-app/App.tsx
+++ quiz-app/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, Button, TouchableOpacity, View } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
@@ -8,7 +8,8 @@ export default function App() {
   return (
     <SafeAreaView>
       <View style={styles.container}>
-      <View style={styles.topContainer}>
+        <View style={styles.entireColumn}>
+        <View style={styles.topContainer}>
       <View>
         <Text style={styles.introText} >Hi, Welcome</Text>
         
@@ -18,13 +19,42 @@ export default function App() {
         
       </View>
         
-          <Ionicons name="person-circle" size ={50}  ></Ionicons>
+        <Ionicons name="person-circle" size ={50}  ></Ionicons>
         
        
         
         
      
       </View>
+
+      <View style={styles.emptyHBox}>
+
+      </View>
+      
+      <Text>
+        Real exams
+      </Text>
+
+      <View style={styles.realExams}>
+        <View style={styles.examRow}>
+          <View>
+          <Text style={styles.textStyleExam}>Your available exams</Text>
+        <Text style={styles.textStyleExam2}>3 exams</Text>
+          </View>
+          <View style={styles.emptyBox1}>
+        
+      </View>
+         
+         <TouchableOpacity ><Ionicons name="md-arrow-forward" size={32}></Ionicons></TouchableOpacity>
+          
+          
+       
+        </View>
+        
+
+      </View>
+        </View>
+        
     </View>
     </SafeAreaView>
     
@@ -37,15 +67,20 @@ const styles = StyleSheet.create({
     flexDirection:"column",
     height: '100%',
     
+    
   },
   topContainer: {
-    marginTop: 25,
-    marginLeft: 25,
-    marginRight: 25,
+  
     flexDirection:'row',
     alignContent: "space-between",
     width: '100%',
     height: 60
+  },
+  entireColumn: {
+    marginTop: 20,
+    flexDirection:"column",
+    marginLeft: 25,
+    marginRight: 25,
   },
   introText: {
     fontSize: 25
@@ -62,11 +97,42 @@ const styles = StyleSheet.create({
   emptyBox: {
     width: 150
   },
+  emptyHBox: {
+    width: '100%',
+    height: 50,
+
+  },
+  emptyBox1: {
+    width: 100
+  },
   circle: {
     width: 50,
     height: 50,
     borderRadius: 999,
     backgroundColor: "#FF0000",
     alignContent: "center"
+  },
+  realExams: {
+    height: 100,
+    backgroundColor: "#509870",
+    borderRadius: 20,
+    marginTop: 10
+  
+  }, 
+  examRow: {
+    flexDirection: "row",
+    padding: 20,
+    alignContent: 'space-between'
+
+  },
+  textStyleExam: {
+    fontSize: 18,
+    color: "#FFFFFF",
+    fontWeight: '800'
+
+  },
+  textStyleExam2: {
+    fontSize: 14,
+    color: "#FFFFFF"
   }
 });
