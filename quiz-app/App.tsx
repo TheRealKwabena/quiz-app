f2d49569af6b7b37dbd991ba378ec002395baa56
Kwabena Asare <kwabenaa@net.chalmmers.se>
2023-12-07 16:41:36 +0100

    feat: welcome message and user icon added to the home screen

--- quiz-app/App.tsx
+++ quiz-app/App.tsx
@@ -1,33 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.textl}>
-        <Text>
-    ddfvdkfvdkvdfkv
-        </Text>
-        <Text>
-dfvjd;lvjdvjdvjd;fjvdfvjklj
-        </Text>
-
-        <Text>Norbert</Text>
-
-        <Text>Jeric</Text>
+    <SafeAreaView>
+      <View style={styles.container}>
+      <View style={styles.topContainer}>
+      <View>
+        <Text style={styles.introText} >Hi, Welcome</Text>
+        
+        <Text style={styles.introText1} >Great to see you again!</Text>
+      </View>
+      <View style={styles.emptyBox}>
+        
+      </View>
+        
+          <Ionicons name="person-circle" size ={50}  ></Ionicons>
+        
+       
+        
+        
+     
       </View>
     </View>
+    </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#FAF5EF",
+    flexDirection:"column",
+    height: '100%',
+    
+  },
+  topContainer: {
+    marginTop: 25,
+    marginLeft: 25,
+    marginRight: 25,
+    flexDirection:'row',
+    alignContent: "space-between",
+    width: '100%',
+    height: 60
+  },
+  introText: {
+    fontSize: 25
 
   },
-  textl: {
-    marginTop: 100,
-    display: "flex",
-    flexDirection: "row"
+  introText1: {
+    marginTop: 10,
+    fontSize: 15
+
+  },
+  userButton: {
+    color: "#FF0000"
+  },
+  emptyBox: {
+    width: 150
+  },
+  circle: {
+    width: 50,
+    height: 50,
+    borderRadius: 999,
+    backgroundColor: "#FF0000",
+    alignContent: "center"
   }
 });
