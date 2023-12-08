bb198ab051f72c729340d399843a7a07f547f719
UgurAbasov <ugurabbasov65@gmail.com>
2023-12-08 20:01:30 +0400

    added pages and components

---
+++ quiz-app/pages/Home.tsx
@@ -0,0 +1,162 @@
+import React from 'react';
+import { Alert, SafeAreaView, StyleSheet, Text, Button, TouchableOpacity, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
+import { Ionicons } from '@expo/vector-icons';
+export default function Home() {
+
+  return (
+ <SafeAreaView>
+
+        <View style={styles.entireColumn}>
+        <View style={styles.topContainer}>
+      <View>
+        <Text style={styles.introText} >Hi, Welcome</Text>
+        <Text style={styles.introText1} >Great to see you again!</Text>
+      </View>
+      <View style={styles.emptyBox}>
+      </View>
+        
+      <Ionicons name="person-circle" size={54} color="black" />        
+       
+        
+        
+     
+      </View>
+
+
+      <View style={styles.joinBlock}>
+        <Text style={styles.wordInBlock}>You want to join for quizz?</Text>
+        <View style={styles.joinToQuizz}>
+        <Button
+        title="Press me"
+        onPress={() => Alert.prompt('Write code')}
+        color="black"
+      />
+      </View>
+
+
+      <View style={styles.emptyHBox}></View>
+      <Text>
+        Real exams
+      </Text>
+
+      <TouchableOpacity>
+      <View style={styles.realExams}>
+        <View style={styles.examRow}>
+          <View>
+          <Text style={styles.textStyleExam}>Your available exams</Text>
+        <Text style={styles.textStyleExaM}>3 exams</Text>
+          </View>
+          <View style={styles.emptyBox1}>
+        
+      </View>
+         
+         <AntDesign name="arrowright" style={styles.arrowIcon} size={24} color="black" />
+      
+        </View>
+
+      </View>
+      </TouchableOpacity>
+        </View>
+
+    </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FAF5EF",
+    flexDirection:"column",
+    height: '100%',
+  },
+  topContainer: {
+    flexDirection:'row',
+    alignContent: "space-between",
+    width: '100%',
+    height: 60
+  },
+  entireColumn: {
+    marginTo: 20,
+    flexDirection:"column",
+    marginLeft: 25,
+    marginRight: 25,
+  },
+  introText: {
+    fontSize: 25
+  },
+  introText1: {
+    marginTop: 10,
+    fontSize: 15
+
+  },
+  userButton: {
+    color: "#FF0000"
+  },
+  emptyBox: {
+    width: 150,
+  },
+  emptyHBox: {
+    width: '100%',
+    height: 50,
+
+  },
+  emptyBox1: {
+    width: 100
+  },
+  circle: {
+    width: 50,
+    height: 50,
+    borderRadius: 999,
+    backgroundColor: "#FF0000",
+    alignContent: "center"
+  },
+  realExams: {
+    height: 100,
+    backgroundColor: "#509870",
+    borderRadius: 20,
+    marginTop: 10
+  }, 
+  examRow: {
+    flexDirection: "row",
+    padding: 20,
+    alignContent: 'space-between'
+  },
+  textStyleExam: {
+    fontSize: 18,
+    color: "#FFFFFF",
+    fontWeight: '800'
+  },
+  joinBlock: {
+    backgroundColor: "#DF8E52",
+    width: 338,
+    height: 100,
+    marginLeft: 'auto',
+    marginRight: "auto",
+    borderRadius: 20,
+    marginTop: 50,
+  },
+  wordInBlock: {
+    marginLeft: 'auto',
+    marginRight: "auto",
+    fontSize: 20,
+    color: "white",
+    marginTop: 10,
+  },
+  joinToQuizz: {
+    backgroundColor: "white",
+    width: '40%',
+    marginLeft: 'auto',
+    marginRight: "auto",
+    borderRadius: 20,
+    marginTop: 10,
+  },
+  textStyleExaM: {
+    fontSize: 14,
+    color: "#FFFFFF",
+  },
+  arrowIcon: {
+    marginTop: 'auto',
+    marginEnd: 'auto'
+  }
+})
